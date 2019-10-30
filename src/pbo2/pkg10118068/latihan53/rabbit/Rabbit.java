bc1da0013cccc6e96802ce3593a5749be4fdb211
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-31 05:05:08 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan53/rabbit/Rabbit.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan53.rabbit;
+
+/**
+ *	
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis objek untuk mendapatkan informasi
+ * hewan seperti kelinci
+ */
+public class Rabbit extends Animal {
+    private String color, name;
+
+    public Rabbit(String name, boolean veg, String food, int legs, String color) {
+        super(veg, food, legs);
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/pbo2/pkg10118068/latihan53/rabbit/Tester.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan53.rabbit;
+
+/**
+ *	
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis objek untuk mendapatkan informasi
+ * hewan seperti kelinci
+ */
+public class Tester {
+
+    private static String name;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit("Peter", false, "grass", 4, "grey");
+        name = rabbit.getName();
+        
+        System.out.println("Hello, His name is " + name);
+        System.out.println(name + " is Vegetarian? " + rabbit.isVegetarian());
+        System.out.println(name + " eats " + rabbit.getEats());
+        System.out.println(name + " has " + rabbit.getNoOfLegs() + " legs");
+        System.out.println(name + " color is " + rabbit.getColor());
+    }
+    
+}
